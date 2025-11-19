3d56908139270fc6d5566fa5dea6c6d759d5644f
Roman Vish <romanv7@byu.edu>
2025-11-19 14:45:28 -0700

    Update deploy settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const isProjectPage = true; // set to false if using username.github.io root
+const repo = "NASIT2026";
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  assetPrefix: isProd && isProjectPage ? `/${repo}/` : "",
+  basePath: isProd && isProjectPage ? `/${repo}` : "",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
   
 };
 
