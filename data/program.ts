f9634dd2e7203390d8c98e5f802a4099d6f739ea
Roman Vish <romanv7@byu.edu>
2026-06-01 17:13:48 -0600

    Update calendar and program

--- data/program.ts
+++ data/program.ts
@@ -3,19 +3,123 @@ import { Presentation } from "@/utils/types";
 export const presentations: Presentation[] = [
   {
     id: "p1",
-    title: "Coming Soon",
-    speakers: ["Coming soon"],
+    title: "Short Tutorial",
+    speakers: ["Alex Sprintson"],
     // abstract:
-      // "An overview of systems research enabling rapid scientific discovery.",
-    // time: "Day 1 — 9:00–10:00",
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Monday — 8:30-10:00",
     // slides: "https://example.com/slides/p1.pdf",
     // video: "https://example.com/videos/p1",
   },
-  // {
-  //   id: "p2",
-  //   title: "Long Tutorial: How to build an ML Pipeline for Baseball data",
-  //   speakers: ["Willie Harrison"],
-  //   abstract: "Patterns for building resilient ML data pipelines.",
-  //   time: "Day 1 — 10:30–12:30",
-  // },
+  {
+    id: "p2",
+    title: "An Introduction to Rate-Distortion-Perception Theory",
+    speakers: ["Jun Chen"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Monday — 10:30-12:00 and 1:30-3:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p3",
+    title: "Communicating Short Messages With and Without Feedback",
+    speakers: ["Rick Wesel"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Monday — 3:30-5:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p4",
+    title: "Quantum Low-Density Parity-Check Codes: Constructions and Decoders",
+    speakers: ["Henry Pfister"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Tuesday — 8:30-10:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p5",
+    title: "Theory and Practice of Diffusion Models",
+    speakers: ["Lalitha Sanka"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Tuesday — 10:30-12:00 and 1:30-3:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p6",
+    title: "Optical List Decoding",
+    speakers: ["Venkat Guruswami"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Tuesday — 3:30-5:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p7",
+    title: "Semantic Coding",
+    speakers: ["Aylin Yener"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Wednesday — 8:30-10:00 and 10:30-12:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p8",
+    title: "Approximate Message Passing Algorithms",
+    speakers: ["Cynthia Rush"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Thursday — 8:30-10:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p9",
+    title: "Long Tutorial",
+    speakers: ["Hamed Hassani"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Thursday — 10:30-12:00 and 1:30-3:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p10",
+    title: "Short Tutorial",
+    speakers: ["Matthieu Bloch"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Thursday — 3:30-5:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p11",
+    title: "Improving Generalization, Robustness, and Reliability in Machine Learning:  a Coding-Theoretic Approach",
+    speakers: ["Mohammad Maddah-Ali"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Friday — 8:30-10:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  {
+    id: "p12",
+    title: "Quickest Change Detection and its Application to Learning in Nonstationary Environments",
+    speakers: ["Venu Veeravalli"],
+    // abstract:
+    //   "n overview of systems research enabling rapid scientific discovery.",
+    time: "Friday — 10:30-12:00",
+    // slides: "https://example.com/slides/p1.pdf",
+    // video: "https://example.com/videos/p1",
+  },
+  
 ];
